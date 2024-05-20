86ad44ed74cbded236961596559e6d8309b38588
CY-H320 <b11705021@ntu.edu.tw>
2024-05-20 21:40:09 +0800

    before python

--- resources/views/visitors/publicCreate.blade.php
+++ resources/views/visitors/publicCreate.blade.php
@@ -5,13 +5,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NTU Dormitory Admin</title>
+    <title>台灣大學住宿管理系統</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">NTU Dormotory Admin</a>
+        <a class="navbar-brand" href="{{ url('/') }}">台灣大學住宿管理系統</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
